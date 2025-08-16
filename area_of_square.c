d3edf0061626b0b4d947bd4bc5a844226f26280f
ashweeni-b <bhardeashweeni05@gmail.com>
2025-08-16 21:25:07 +0530

    Added program to calculate area of square if the side is given

---
+++ area_of_square.c
@@ -0,0 +1,13 @@
+// This program calculates area of a square if the side is given
+
+#include <stdio.h>
+
+int main() {
+    float side = 5.0;
+
+    float area = side * side;	// Calculates the area of square
+
+    printf("The area of square is %f", area);	// Outputs the area
+
+    return 0;
+}
